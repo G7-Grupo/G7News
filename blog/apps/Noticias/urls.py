f9a0ab1fa21c16558f46aa36d87a563d28f35467
Agustin Roman <romanagustinuser@gmail.com>
2024-10-13 23:15:12 -0300

    intento de arreglar search modified:   apps/Noticias/urls.pymodified:   apps/Noticias/views.pymodified:   templates/Noticias/search.html

--- blog/apps/Noticias/urls.py
+++ blog/apps/Noticias/urls.py
@@ -1,17 +1,35 @@
 
 from django.urls import path
-from . import views
-from .views import UpdatePostView
+# from . import views
+from .views import search, blogs, blogs_comments, Delete_Blog_Post, add_blogs, UpdatePostView
+# si se importan funciones y clases solo hay que poner el nombre como segundo parametro de path
 
 app_name = 'Noticias'
 
 urlpatterns = [
-#     blogs
+    path('', blogs, name='blogs'),
+    path('blog/<str:slug>/', blogs_comments, name='blogs_comments'), # da igual usar str:slug o slug:slug
+    path('add_blogs/', add_blogs, name='add_blogs'),
+    path('edit_blog_post/<str:slug>/', UpdatePostView.as_view(), name='edit_blog_post'), # para identificar por ID los post 
+    path('delete_blog_post/<slug:slug>/', Delete_Blog_Post, name='delete_blog_post'),
+    path('search/', search, name='search'),
+]
+""" 
+urlpatterns = [
+    # blogs
     path("", views.blogs, name="blogs"),
     path("blog/<str:slug>/", views.blogs_comments, name="blogs_comments"),
     path("add_blogs/", views.add_blogs, name="add_blogs"),
     path("edit_blog_post/<str:slug>/", UpdatePostView.as_view(), name="edit_blog_post"),
     path("delete_blog_post/<str:slug>/", views.Delete_Blog_Post, name="delete_blog_post"),
     path("search/", views.search, name="search"),
-
 ]
+urlpatterns = [
+    path('', blogs, name='blogs'),
+    path('blog/<slug:slug>/', blogs_comments, name='blogs_comments'), # da igual usar str:slug o slug:slug
+    path('add_blogs/', add_blogs, name='add_blogs'),
+    path('edit_blog_post/<int:pk>/', UpdatePostView.as_view(), name='edit_blog_post'), # para identificar por ID los post 
+    path('delete_blog_post/<slug:slug>/', Delete_Blog_Post, name='delete_blog_post'),
+    path('search/', search, name='search'),
+    
+] """
--- blog/apps/Noticias/views.py
+++ blog/apps/Noticias/views.py
@@ -9,21 +9,36 @@ from django.views.generic import UpdateView
 from django.contrib import messages
 
 
-def blogs(request):
+""" def blogs(request):
     posts = BlogPost.objects.all()
     posts = BlogPost.objects.filter().order_by('-dateTime')
-    return render(request, "blog.html", {'posts':posts})
+    return render(request, "blog.html", {'posts':posts}) """
 
+def blogs(request):
+    posts = BlogPost.objects.all().order_by('-dateTime')  # Simplificado para obtener todos los posts ordenados
+    return render(request, "blog.html", {'posts': posts})
+""" 
 def blogs_comments(request, slug):
     post = BlogPost.objects.filter(slug=slug).first()
     comments = Comment.objects.filter(blog=post)
-    if request.method=="POST":
+    if request.method =="POST":
         user = request.user
         content = request.POST.get('content','')
         blog_id =request.POST.get('blog_id','')
         comment = Comment(user = user, content = content, blog=post)
         comment.save()
     return render(request, "blog_comments.html", {'post':post, 'comments':comments})
+ """
+def blogs_comments(request, slug):
+    post = BlogPost.objects.filter(slug=slug).first()
+    comments = Comment.objects.filter(blog=post)
+    if request.method == "POST":
+        user = request.user
+        content = request.POST.get('content', '')
+        comment = Comment(user=user, content=content, blog=post)
+        comment.save()
+    return render(request, "blog_comments.html", {'post': post, 'comments': comments})
+
 
 def Delete_Blog_Post(request, slug):
     posts = BlogPost.objects.get(slug=slug)
@@ -31,7 +46,7 @@ def Delete_Blog_Post(request, slug):
         posts.delete()
         return redirect('/')
     return render(request, 'delete_blog_post.html', {'posts':posts})
-
+# search 1
 def search(request):
     if request.method == "POST":
         searched = request.POST['searched']
@@ -39,6 +54,30 @@ def search(request):
         return render(request, "search.html", {'searched':searched, 'blogs':blogs})
     else:
         return render(request, "search.html", {})
+
+
+# search 2
+""" def search(request):
+    searched = request.GET.get('q', '')  # Cambiado a GET para facilitar la búsqueda
+    blogs = BlogPost.objects.filter(title__icontains=searched) if searched else BlogPost.objects.none()
+    return render(request, "search.html", {'searched': searched, 'blogs': blogs})
+ """
+# search 3
+""" def search(request):
+    query = request.GET.get('q', '')  # Obtener el término de búsqueda desde la URL
+    if query:
+        blogs = BlogPost.objects.filter(title__icontains=query)  # Filtrar blogs que contienen el término de búsqueda
+        searched = True
+    else:
+        blogs = BlogPost.objects.none()  # Si no hay búsqueda, no se mostrarán resultados
+        searched = False
+
+    context = {
+        'blogs': blogs,
+        'searched': searched,
+    }
+    return render(request, "search.html", context)
+ """
     
 @login_required(login_url = '/login')
 def add_blogs(request):
