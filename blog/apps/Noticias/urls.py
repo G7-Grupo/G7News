3c1e4961fc30dcc134371fee1ca8a80dcd08d2fc
Emi-debug <rojasemilio107@gmail.com>
2024-10-15 11:01:37 -0300

    delete blogs

--- blog/apps/Noticias/urls.py
+++ blog/apps/Noticias/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path("blog/<str:slug>/", views.blogs_comments, name="blogs_comments"),
     path("add_blogs/", views.add_blogs, name="add_blogs"),
     path("edit_blog_post/<str:slug>/", UpdatePostView.as_view(), name="edit_blog_post"),
-    path("delete_blog_post/<str:slug>/", views.Delete_Blog_Post, name="delete_blog_post"),
+    path("delete_blog_post/<int:pk>/", views.Delete_Blog_Post, name="delete_blog_post"),
     path("search/", views.search, name="search"),
 
 ]
