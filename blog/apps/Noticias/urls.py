86fd223dc7510b143de2521e9697dd098476088d
TCHPua <pabloulisesaguirre@gmail.com>
2024-10-16 21:00:33 -0300

    Delete Post - Funcionando

--- blog/apps/Noticias/urls.py
+++ blog/apps/Noticias/urls.py
@@ -8,10 +8,11 @@ app_name = 'apps.Noticias'
 urlpatterns = [
 #     blogs
     path("", views.blogs, name="blogs"),
-    path("blog/<str:slug>/", views.blogs_comments, name="blogs_comments"),
+    path("blog/<slug:slug>/", views.blogs_comments, name="blogs_comments"),
     path("add_blogs/", views.add_blogs, name="add_blogs"),
     path("edit_blog_post/<str:slug>/", UpdatePostView.as_view(), name="edit_blog_post"),
     path("delete_blog_post/<int:pk>/", views.Delete_Blog_Post, name="delete_blog_post"),
+    path("delete_comment/<int:comment_id>/", views.Delete_comment, name="delete_comment"),
     path("search/", views.search, name="search"),
 
 ]
--- blog/apps/Noticias/views.py
+++ blog/apps/Noticias/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth  import authenticate,  login, logout
 from .models import *
@@ -27,6 +27,18 @@ def blogs_comments(request, slug):
     return render(request, "Noticias/blog_comments.html", {'post':post, 'comments':comments})
 
 @login_required(login_url='/login')
+def Delete_comment(request, comment_id):
+    
+    comment = get_object_or_404(Comment, id=comment_id, user=request.user)
+        
+    if request.method == "POST":
+        comment.delete()  
+        return redirect('/')
+        #return redirect('blog_comments', slug = ?) 
+
+    return render(request, 'Noticias/delete_comment.html', {'comment': comment})
+
+@login_required(login_url='/login')
 def Delete_Blog_Post(request, pk):
     posts = BlogPost.objects.get(id=pk)
     if request.method == "POST":
@@ -42,22 +54,6 @@ def search(request):
     else:
         return render(request, "search.html", {})
 
-"""    
-@login_required(login_url = '/login')
-def add_blogs(request):
-    if request.method=="POST":
-        form = BlogPostForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            blogpost = form.save(commit=False)
-            blogpost.author = request.user
-            blogpost.save()
-            obj = form.instance
-            alert = True
-            return render(request, "Noticias/blog.html",{'obj':obj, 'alert':alert})
-    else:
-        form=BlogPostForm()
-    return render(request, "Noticias/add_blogs.html", {'form':form})
-"""
 @login_required(login_url='/login')
 def add_blogs(request):
     if request.method == "POST":
