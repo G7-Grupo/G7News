737642c4f4f60a90705c9c6388530813368bfdba
Emi-debug <rojasemilio107@gmail.com>
2024-10-18 12:57:50 -0300

    Comments

--- blog/apps/Noticias/urls.py
+++ blog/apps/Noticias/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 from . import views
-from .views import UpdatePostView, AddCategoriaView
+from .views import UpdatePostView, AddCategoriaView,UpdateCommentView
 
 app_name = 'apps.Noticias'
 
@@ -10,9 +10,12 @@ urlpatterns = [
     path("", views.blogs, name="blogs"),
     path("blog/<str:slug>/", views.blogs_comments, name="blogs_comments"),
     path("add_blogs/", views.add_blogs, name="add_blogs"),
-    path("add_Categoria/",AddCategoriaView.as_view(), name="add_Categoria"),
     path("edit_blog_post/<str:slug>/", UpdatePostView.as_view(), name="edit_blog_post"),
-    path("delete_blog_post/<int:pk>/", views.Delete_Blog_Post, name="delete_blog_post"),
+    path("add_Categoria/",AddCategoriaView.as_view(), name="add_Categoria"),
+    path("delete_comment/<int:comment_id>/", views.Delete_comment, name="delete_comment"),
+    path("edit_comment/<int:comment_id>/", UpdateCommentView.as_view(), name="edit_comment"),
+    
+   
     path("search/", views.search, name="search"),
 
 ]
--- blog/apps/Noticias/views.py
+++ blog/apps/Noticias/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView, CreateView
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from .models import *
 from django.contrib.auth.decorators import login_required
@@ -26,12 +26,35 @@ def blogs_comments(request, slug):
     return render(request, "Noticias/blog_comments.html", {'post':post, 'comments':comments})
 
 @login_required(login_url='/login')
+def Delete_comment(request, comment_id):
+    
+    comment = get_object_or_404(Comment, id=comment_id, user=request.user)
+        
+    if request.method == "POST":
+        comment.delete()  
+        return redirect(reverse('Noticias:blogs_comments', kwargs={'slug': comment.blog.slug}))
+
+@login_required(login_url='/login')
 def Delete_Blog_Post(request, pk):
     posts = BlogPost.objects.get(id=pk)
     if request.method == "POST":
         posts.delete()
         return redirect('/')
     return render(request, 'Noticias/delete_blog_post.html', {'posts':posts})
+
+
+class UpdateCommentView(UpdateView):
+    model = Comment
+    template_name = 'Noticias/edit_comment.html'
+    fields = ['content']
+
+    def get_object(self, queryset=None):
+        comment_id = self.kwargs.get('comment_id')
+        return get_object_or_404(Comment, id=comment_id)
+
+    def form_valid(self, form):
+        form.save()
+        return redirect(reverse('Noticias:blogs_comments', kwargs={'slug': self.object.blog.slug}))
 
 def search(request):
     if request.method == "POST":
